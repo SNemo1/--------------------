833e575f494249c3bfa5a36233fe4e5b9703564a
Mensis <vladlugoviy08@gmail.com>
2024-10-20 22:19:53 +0300

    Фінаьні правки

--- scripts/animations.js
+++ scripts/animations.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function scrollPage(className) {
-    const element = document.querySelector(`.${className}`);
+    const element = document.querySelector(`${className}`);
     if (element) {
         element.scrollIntoView({
             behavior: "smooth"
